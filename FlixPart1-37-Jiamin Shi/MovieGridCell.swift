198e752ae73bafa8a738c2e692ae9498cb2f4d93
JiaminShi1226 <shijiamin0324@gmail.com>
2022-03-04 17:46:04 -0500

    Part2 upload

---
+++ FlixPart1-37-Jiamin Shi/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  FlixPart1-37-Jiamin Shi
+//
+//  Created by JiaminShi on 3/4/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
